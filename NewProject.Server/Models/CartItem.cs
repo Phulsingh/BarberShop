376497ae3bd9aed1bec51601761d2f73d2048daf
Phulsing <phulsing@deccansoft.net>
2025-10-11 09:45:12 +0530

    Add To Cart API Created .

---
+++ NewProject.Server/Models/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace NewProject.Server.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ServiceId { get; set; }
+        public BarbarServices Service { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
